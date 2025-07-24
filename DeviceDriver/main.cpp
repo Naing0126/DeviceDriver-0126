420333090dd1f38a4a98bf04311d23b89a278132
naing.yoon <naing.yoon@samsung.com>
2025-07-24 15:15:37 +0900

    [Feature] Pass ReadFromHW

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,9 +1,20 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	unsigned char read(long address) override {
+		return 0;
+	}
+	void write(long address, unsigned char data) override {
+
+	}
+
+};
+
 TEST(DeviceDriver, ReadFromHW) {
 	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
+	FlashMemoryDevice* hardware = new MockFlashMemoryDevice;
 	DeviceDriver driver{ hardware };
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
